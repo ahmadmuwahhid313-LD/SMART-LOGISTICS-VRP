ecb4ab852ded0ee6cff618f89d8e08ce28471a2c
ahmadmuwahhid313-LD <ahmadmuwahhid313@gmail.com>
2026-05-23 15:44:32 +0700

    initial commit

---
+++ aplikasi-vrp.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Smart Logistic Distribution")
+st.write("Optimation Distribution Logistic Sistem")
+
+# Initialize Git repository
+# git init
+# git add .
+# git commit -m "initial commit"
+# git remote add origin https://github.com/ahmadmuwahhid313-LD/SMART-LOGISTIC-PROJECT.git
+# git branch -M main
+# git push -u origin main
